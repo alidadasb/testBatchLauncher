8bd3b3174dedeff8cbee82c85bd0ad037771dd11
Alidad <programmer@dorisa.com>
2013-03-15 00:33:48 -0400

    first commit, testing bactch-launcher with grails 2

---
+++ target/batchl-0.1/batchl.bat
@@ -0,0 +1,4 @@
+@echo off
+java -jar batchl-0.1.jar %*
+REM java -cp .;war/WEB-INF/classes;batchl-0.1.jar; -jar batchl-0.1.jar %*
+REM exit /b %errorlevel%
---
+++ target/batchl-0.1/batchl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+java -jar batchl-0.1.jar $*
+#java -cp .:war/WEB-INF/classes:batchl-0.1.jar: -jar batchl-0.1.jar $*
+#exit $?
